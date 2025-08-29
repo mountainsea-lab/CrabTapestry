62b65cdca15f1819aab3e9707b1859e08e9a5e7b
mountainsea-lab <w1999wtw3537@gmail.com>
2025-08-29 22:13:35 +0800

    BarterIngestor init

--- crab-data-event/src/ingestion/barter_ingestor.rs
+++ crab-data-event/src/ingestion/barter_ingestor.rs
@@ -1,1 +1,7 @@
+use crate::ingestion::types::PublicTradeEvent;
+use crossbeam::channel::Sender;
 
+/// BarterIngestor acquires real-time trade data events from the exchange
+pub struct BarterIngestor {
+    sender: Sender<PublicTradeEvent>, // trade data transfer channel
+}
