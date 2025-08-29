068e877707434e300fbbcd5787f1b3b2f6b7d86a
mountainsea-lab <w1999wtw3537@gmail.com>
2025-08-29 21:57:05 +0800

    Define the top trait of the data event business

--- crab-data-event/src/ingestion/mod.rs
+++ crab-data-event/src/ingestion/mod.rs
@@ -1,2 +1,15 @@
+use std::sync::Arc;
+
 mod types;
 mod barter_ingestor;
+
+/// The common DataEvent Trait, if data sources implement it can own event ability.
+pub trait DataEvent {
+    fn id(&self) -> String;
+    fn timestamp(&self) -> u64;
+}
+
+// Define the data acquisition behavior trait
+pub trait Ingestor<E: DataEvent> {
+    fn start(self: Arc<Self>);
+}
