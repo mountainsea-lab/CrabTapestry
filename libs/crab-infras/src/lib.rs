5ee3dccf6dd25acab986fae57587cb6c0839cbc9
mountainsea-lab <w1999wtw3537@gmail.com>
2025-08-30 17:12:40 +0800

    create utils

--- libs/crab-infras/src/lib.rs
+++ libs/crab-infras/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cache;
 pub mod db;
 pub mod external;
+mod utils;
---
+++ libs/crab-infras/src/utils.rs
@@ -0,0 +1,13 @@
+use bb8_redis::{bb8, RedisConnectionManager};
+
+pub async fn create_redis_pool(redis_url: &str) -> Result<bb8::Pool<RedisConnectionManager>> {
+    let manager = RedisConnectionManager::new(redis_url)?;
+    let pool = bb8::Pool::builder()
+        .max_size(200)
+        .min_idle(Some(20))
+        .max_lifetime(Some(std::time::Duration::from_secs(60 * 15))) // 15 minutes
+        .idle_timeout(Some(std::time::Duration::from_secs(60 * 5))) // 5 minutes
+        .build(manager)
+        .await?;
+    Ok(pool)
+}
