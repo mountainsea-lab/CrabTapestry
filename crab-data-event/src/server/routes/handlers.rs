e80905e06bbe4db9d97627bce361cc2f85ea8313
www1 <w1999wtw3537@sina.com>
2025-09-01 14:17:07 +0800

    log handler,base info handler

---
+++ crab-data-event/src/server/routes/handlers.rs
@@ -0,0 +1,21 @@
+pub mod log_handlers;
+
+pub fn index() -> &'static str {
+    "Welcome to mini bot!"
+}
+
+pub fn ping() -> &'static str {
+    "ping pong!"
+}
+
+pub fn version() -> &'static str {
+    "crab-data-event version 0.1.0"
+}
+
+pub fn sysinfo() -> &'static str {
+    "sysinfo info: hello , I am a crab tapestry data event server"
+}
+
+pub fn health() -> &'static str {
+    "if you ask: hao are you,oh I am ok"
+}
