4b24af6448df4d7c711e33441383486c6613ef81
www1 <w1999wtw3537@sina.com>
2025-09-04 15:03:32 +0800

    defined Deduplicatable trait

--- crab-hmds/src/ingestor/dedup/mod.rs
+++ crab-hmds/src/ingestor/dedup/mod.rs
@@ -1,2 +1,11 @@
+use std::sync::Arc;
+
 pub mod deduplicator;
 
+/// 定义一个 trait，泛型去重对象必须实现生成唯一 key
+/// defining a trait that deduplicatable objects must implement generating unique key
+pub trait Deduplicatable {
+    /// 返回唯一 key，建议使用 Arc<str> 或组合元组，避免 heap 分配
+    /// return unique key, it's recommended to use Arc<str> or a combination of tuples to avoid heap allocation
+    fn unique_key(&self) -> Arc<str>;
+}
