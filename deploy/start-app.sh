b779170fc795f4f20a679f7c87dd159940ff3a9a
www1 <w1999wtw3537@sina.com>
2025-09-25 15:39:46 +0800

    excute sh,bash both support

--- deploy/start-app.sh
+++ deploy/start-app.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# ===============================
-# 启动服务
-# ===============================
-if [ $# -eq 0 ]; then
-  echo "[start-app.sh] No command provided, exiting."
-  exit 1
+# 如果系统有 bash 就用 bash，否则用 sh
+if command -v bash >/dev/null 2>&1; then
+    SHELL_EXEC=bash
+else
+    SHELL_EXEC=sh
 fi
 
-echo "[start-app.sh] Starting service: $*"
-exec "$@"
+echo "[start-app.sh] Using shell: $SHELL_EXEC"
+
+if [ $# -eq 0 ]; then
+    echo "[start-app.sh] No command provided, exiting."
+    exit 1
+fi
+
+exec "$SHELL_EXEC" -c "$*"
