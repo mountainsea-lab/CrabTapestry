53579957d7ae4a31f9f01c2cca6587cb00e17ba4
mountainsea-lab <w1999wtw3537@gmail.com>
2025-10-05 22:28:08 +0800

    trader_handlers add functions

--- crab-strategy/src/server/routes/handlers/trader_handlers.rs
+++ crab-strategy/src/server/routes/handlers/trader_handlers.rs
@@ -1,11 +1,42 @@
-pub fn enable_trading() -> &'static str {
-    "Welcome to crab-strategy!"
+use crate::global;
+use crate::server::response::ErrorResponse;
+use warp::{Rejection, Reply, reply};
+
+/// 开启交易
+pub async fn enable_trading() -> &'static str {
+    let trader = global::get_crab_trader();
+
+    if trader.enable_trading().await.is_ok() {
+        "crab-strategy enable trading success..."
+    } else {
+        "crab-strategy enable trading failed!"
+    }
+}
+/// 停止交易
+pub async fn disable_trading() -> &'static str {
+    let trader = global::get_crab_trader();
+
+    if trader.enable_trading().await.is_ok() {
+        "crab-strategy disable trading success..."
+    } else {
+        "crab-strategy disable trading failed!"
+    }
 }
 
-pub fn disable_trading() -> &'static str {
-    "crab-strategy version 0.1.0"
-}
+/// 获取系统运行状态
+pub async fn get_status() -> Result<impl Reply, Rejection> {
+    // ✅ 获取全局 CrabTrader
+    let trader = global::get_crab_trader();
 
-pub fn get_status() -> &'static str {
-    "ping pong!"
+    // ✅ 异步获取状态
+    match trader.get_status().await {
+        Ok(status) => {
+            // 返回 JSON 响应
+            Ok(reply::json(&status))
+        }
+        Err(err) => {
+            // 发生错误时返回 HTTP 500
+            Err(warp::reject::custom(ErrorResponse { message: err.to_string() }))
+        }
+    }
 }
--- crab-strategy/src/server/strategy_flow.rs
+++ crab-strategy/src/server/strategy_flow.rs
@@ -4,9 +4,9 @@ use ms_tracing::tracing_utils::internal::info;
 use std::sync::Arc;
 
 pub async fn start_strategy_flow() {
-    // 2️⃣ 初始化交易器
+    // 交易器
     let trader = global::get_crab_trader();
-    if trader.enable_trading().await.is_ok() {
-        info!("Trader enable trading");
-    }
+    // if trader.enable_trading().await.is_ok() {
+    //     info!("Trader enable trading");
+    // }
 }
