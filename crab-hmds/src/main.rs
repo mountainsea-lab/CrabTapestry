bcdec6d8518157e84d5e739dec6aedcf0fb41290
www1 <w1999wtw3537@sina.com>
2025-11-05 16:36:21 +0800

    server prod env

--- crab-hmds/src/main.rs
+++ crab-hmds/src/main.rs
@@ -6,11 +6,11 @@ use std::path::Path;
 #[tokio::main]
 pub async fn main() {
     // 1️⃣ 加载本 crate 根目录下的 .env（开发环境用）
-    let manifest_dir = env!("CARGO_MANIFEST_DIR");
-    let env_path = Path::new(manifest_dir).join(".env");
-    if env_path.exists() {
-        let _ = from_path(&env_path);
-    }
+    // let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    // let env_path = Path::new(manifest_dir).join(".env");
+    // if env_path.exists() {
+    //     let _ = from_path(&env_path);
+    // }
     // unsafe {
     //     env::set_var(
     //         "DATABASE_URL",
